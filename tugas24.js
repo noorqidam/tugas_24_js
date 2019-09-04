05b3eae4a7d19156d7a20e6bac896d519965c187
noorqidam <noorqidam@gmail.com>
2019-09-04 09:34:05 +0700

    Tugas 24 JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas24.js
@@ -0,0 +1,7 @@
+function tugas24() {
+  var angka = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66]
+  console.log("sebelumnya :",angka)
+  console.log("Ascending :",angka.sort());
+  console.log("Descending :",angka.reverse());
+}
+tugas24();
